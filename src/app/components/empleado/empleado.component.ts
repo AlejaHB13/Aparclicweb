ea7542b2f1798d43e9f9064e998c5391773d018b
Alejandra <alejandra.hernandezb@konradlorenz.edu.co>
2023-05-27 14:04:37 -0500

    POST , PUT y DELETE CLIENTES
    Se agrega código para editar, borrar y agregar un empleado

--- src/app/components/empleado/empleado.component.ts
+++ src/app/components/empleado/empleado.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { EmpleadoService } from 'src/app/services/empleado.service';
-
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-empleado',
@@ -9,12 +12,88 @@ import { EmpleadoService } from 'src/app/services/empleado.service';
 })
 export class EmpleadoComponent {
   empleadoList: any = [];
-  constructor(private EmpleadoService: EmpleadoService) { }
+  empleadoForm: any = this.formBuilder.group({
+    nombre: '',
+    cedula: '',
+    usuario: '',
+    contrasena: ''
+  })
+
+  editableEmpleado: boolean = false;
+  idEmpleado: any;
+
+  constructor(
+    private EmpleadoService: EmpleadoService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private toastr: ToastrService) { }
+
   getAllempleados() {
     this.EmpleadoService.getAllempleadosData().subscribe((data: {}) => {
       this.empleadoList = data;
     });
   }
+
+  newEmpleadoEntry() {
+    this.EmpleadoService.newEmpleado(this.empleadoForm.value).subscribe(
+      () => {
+        //Redirigiendo a la ruta actual /empleado y recargando la ventana
+        this.router.navigate(['/empleado']).then(() => {
+          this.newMessage('Registro exitoso');
+        })
+      }
+    );
+  }
+
+  updateEmpleadoEntry() {
+    //Removiendo valores vacios del formulario de actualización
+    for (let key in this.empleadoForm.value) {
+      if (this.empleadoForm.value[key] === '') {
+        this.empleadoForm.removeControl(key);
+      }
+    }
+    this.EmpleadoService.updateEmpleado(this.idEmpleado, this.empleadoForm.value).subscribe(
+      () => {
+        //Enviando mensaje de confirmación
+        this.newMessage("Empleado editado");
+      }
+    );
+  }
+
+  toggleEditEmpleado(id: any) {
+    this.idEmpleado = id;
+    console.log(this.idEmpleado)
+    this.EmpleadoService.getOneEmpleado(id).subscribe(
+      data => {
+        this.empleadoForm.setValue({
+          nombre: data.nombre,
+          cedula: data.cedula,
+          usuario: data.usuario,
+          contrasena: data.contrasena
+        });
+      }
+    );
+    this.editableEmpleado = !this.editableEmpleado;
+  }
+
+  deleteEmpleadoEntry(id: any) {
+    console.log(id)
+    this.EmpleadoService.deleteEmpleado(id).subscribe(
+      () => {
+        //Enviando mensaje de confirmación
+        this.newMessage("Empleado eliminado");
+      }
+    );
+  }
+
+  newMessage(messageText: string) {
+    this.toastr.success('Clic aquí para actualizar la lista', messageText)
+      .onTap
+      .pipe(take(1))
+      .subscribe(() => window.location.reload());
+  }
+
+
   ngOnInit() {
     this.getAllempleados();
   }
--- src/app/services/empleado.service.ts
+++ src/app/services/empleado.service.ts
@@ -10,13 +10,35 @@ export class EmpleadoService {
   apiUri = '/api/empleado';
   httpOptions = new HttpHeaders().set('Content-Type', 'application/json');
 
-
-
-
   constructor(private http: HttpClient) { }
 
   getAllempleadosData(): Observable<any> {
     return this.http.get<any>(this.apiUri)
+  }
+
+  updateEmpleado(id: any, data: any): Observable<any> {
+    console.log(data)
+    return this.http.put<any>(
+      this.apiUri + '/' + id,
+      data,
+      { headers: this.httpOptions });
+  }
+
+  getOneEmpleado(id: any): Observable<any> {
+    return this.http.get<any>(
+      this.apiUri + '/' + id,
+      { headers: this.httpOptions });
+  }
+
+  deleteEmpleado(id: any) {
+        return this.http.delete<any>(
+          this.apiUri + "/" + id,
+          { headers: this.httpOptions });
+      }
+    
+
+  newEmpleado(data: any): Observable<any> {
+    return this.http.post<any>(this.apiUri, data, { headers: this.httpOptions });
   }
 
 }
